06975d87dd6ef5f34680b5a63ee7eec55c26500b
Mansurov1990 <mansurov1990@gmail.com>
2021-08-07 00:52:49 -0400

    Created package, created class-add new string

--- src/Farid_Mansurov/Hello_myself.java
+++ src/Farid_Mansurov/Hello_myself.java
@@ -1,7 +1,12 @@
 package Farid_Mansurov;
 
+import java.sql.SQLOutput;
+
 public class Hello_myself {
     public static void main(String[] args) {
-        System.out.println("Farid welcome aboard!");
+        System.out.println("Farid welcome aboard!!!!!!!");
+        System.out.println("meow");
+
+        System.out.println("");
     }
 }
