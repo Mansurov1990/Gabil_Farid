82bfb0dfcb4c0ba6713bab26c28b1023a8bc71fe
gabil89 <56522174+gabil89@users.noreply.github.com>
2021-08-07 00:25:02 -0400

    hello farid i added some3

--- src/Gabil1/Gabil1.java
+++ src/Gabil1/Gabil1.java
@@ -4,6 +4,6 @@ public class Gabil1 {
     public static void main(String[] args) {
         System.out.println("hello farid");
         System.out.println("my second commit");
-        System.out.println("i hope you liked");
+        System.out.println("i hope you liked!");
     }
 }
