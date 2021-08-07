bf4d0ccbc4a8a446f8a3da7ecb715a06b2069604
gabil89 <56522174+gabil89@users.noreply.github.com>
2021-08-07 01:07:58 -0400

    hello farid i added some5

--- src/Gabil1/Gabil1.java
+++ src/Gabil1/Gabil1.java
@@ -7,5 +7,6 @@ public class Gabil1 {
         System.out.println("i hope you liked!");
         System.out.println("hello farid approve from your github application");
         System.out.println("my second commit");
+        System.out.println("my second commit");
     }
 }
